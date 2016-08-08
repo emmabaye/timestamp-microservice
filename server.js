c6e3faeef8bebb183d190cef734ee4d557fc4f88
Emmanuel Abaye <abayeemmanuel@yahoo.com>
2016-08-08 04:56:50 +0100

    No UTC

---
+++ server.js
@@ -0,0 +1,45 @@
+'use strict';
+
+var express = require('express');
+var moment = require('moment');
+
+var app = express();
+
+app.get('/', function(req, res){
+	res.send("<h1>Timestamp Microservice</h1>")
+});
+app.get('/:date', function(req, res){
+
+	//check if natural
+	var n = moment(req.params.date, ["MM DD YYYY", "YYYY MM DD"]);
+
+	if(n.isValid()){
+		
+		return res.json({
+			"unix": n.unix(),
+			"natural": n.format("MMMM DD, YYYY")
+		});
+	}
+
+	//check if unix
+	var m = moment(parseFloat(req.params.date) * 1000);
+	if(m.isValid()){
+		
+		return res.json({
+			"unix": m.unix(),
+			"natural":m.format("MMMM DD, YYYY")
+		});
+	}
+
+	return res.json({
+			"unix": null,
+			"natural":null
+		});
+	
+});
+
+var port = process.env.PORT || 3000;
+app.listen(port, function(){
+	console.log("Node.js is listening on port " + port + "...")
+});
+
